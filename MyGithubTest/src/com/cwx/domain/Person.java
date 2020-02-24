9f2c154f5931ef18a22380e3520d7154f2614b3a
chenwuxin123 <785299033@qq.com>
2020-02-25 01:21:12 +0800

    123123

---
+++ MyGithubTest/src/com/cwx/domain/Person.java
@@ -0,0 +1,7 @@
+package com.cwx.domain;
+
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+}
---
+++ MyGithubTest/src/com/cwx/test/Test.java
@@ -0,0 +1,15 @@
+package com.cwx.test;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer sum1 = 19;
+        Integer sum2 = 19;
+        Integer sum3 = 128;
+        Integer sum4 = 128;
+        System.out.println(sum1==sum2);
+        System.out.println(sum3==sum4);
+    }
+
+
+
+}
